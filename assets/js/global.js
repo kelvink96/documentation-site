14e78977e49ed92aff873dd8210e4a33301c7d3d
Kelvin Kiptum Kiprop <kelvin.kiprop96@gmail.com>
2021-01-30 21:50:30 +0300

    - add page sorter function to set active pages - added jquery - updates on nav and list group styles

--- assets/js/global.js
+++ assets/js/global.js
@@ -7,3 +7,18 @@ function closeNav() {
 	document.getElementById("mySidebar").style.width = "0";
 	document.getElementById("main").style.marginLeft= "0";
 }
+
+function pageTitleSorter() {
+	var url = window.location.href;
+
+	let splitUrl = url.split('/'),
+		composedUrl = splitUrl[splitUrl.length - 1].split('.')[0];
+	composedUrl = composedUrl.replace('%20', ' ');
+
+	$('.sidebar .list-group').find('.list-group-item:contains(' + composedUrl + ')').addClass('active');
+	$('.navbar-nav .nav-item').find('.nav-link:contains(' + composedUrl + ')').addClass('active');
+}
+
+$(() => {
+	pageTitleSorter();
+})
